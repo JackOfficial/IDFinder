ea962eb20be1dd82d853f3df73a5be5d008734c3
Musengimana Jacques <musengimanajacques@gmail.com>
2022-10-27 10:00:32 +0200

    First commit

---
+++ app/Http/Controllers/Auth/SocialController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator, Redirect, Response, File;
+use Socialite;
+use Auth;
+use App\Models\User;
+
+class SocialController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback()
+{
+    try {
+            $user = Socialite::driver('google')->user();
+            $user = User::where('google_id', $user->getId())->first();
+
+            if($user){
+                Auth::login($user);
+                return redirect('/home');
+                // return redirect()->intended();
+            }else{
+                $newUser = User::create([
+                    'name' => $user->getName(),
+                    'email' => $user->getEmail(),
+                    'google_id'=> $user->getId(),
+                    'password' => encrypt('123456dummy'),
+                    'avatar' => $user->getAvatar()
+                ]);
+                Auth::login($newUser);
+                return redirect('/home');
+                // return redirect()->intended();
+            }
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+   }
+}
